3e70d0b292edd899611d4c8990b77ff8e15973be
pprathyusha338 <47249360+pprathyusha338@users.noreply.github.com>
2019-06-27 14:47:46 +0530

    Add files via upload

---
+++ flapp12.js
@@ -0,0 +1,30 @@
+recs=[]
+let ax=50;
+let ay=0;
+ 
+function setup() {
+  b=new bird();
+  
+  createCanvas(450,450);
+  for(i=0;i<1000;i++){
+    rec=new obstacle();
+    recs.push(rec);}
+
+}
+
+
+function draw() {
+  background("black");
+  for (let y of recs){
+    
+  y.move();
+    y.show();}
+    
+ 
+
+    
+  
+   b.show();
+    
+
+}
